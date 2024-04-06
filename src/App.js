b1cb3d78a8e8b5e9171bc886a774ebd31a297912
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-04-06 18:41:48 -0300

    added base components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button 
+      onClick={handleClick}
+      className="square">
+      {value}
+    </button>
+  );
 }
